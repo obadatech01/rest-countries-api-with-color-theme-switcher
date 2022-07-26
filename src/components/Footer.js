cf77fa91069e379b67a7680776507eb6525794c0
Obada Yehia <67144995+obadatech01@users.noreply.github.com>
2022-07-26 23:54:09 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,9 @@
-import { Link } from "react-router-dom";
-
 function Footer() {
   return (
     <>
       <footer className="footer">
-          <p>Challenge by: <Link target="_blank" to="https://www.frontendmentor.io/challenges/rest-countries-api-with-color-theme-switcher-5cacc469fec04111f7b848ca"> Frontend Mentor</Link></p>
-          <p>Coded by: <Link target="_blank" to="https://github.com/obadatech01/rest-countries-api-with-color-theme-switcher"> Obada Yehia</Link></p>
+          <p>Challenge by: <a target="_blank" href="https://www.frontendmentor.io/challenges/rest-countries-api-with-color-theme-switcher-5cacc469fec04111f7b848ca"> Frontend Mentor</a></p>
+          <p>Coded by: <a target="_blank" href="https://github.com/obadatech01/rest-countries-api-with-color-theme-switcher"> Obada Yehia</a></p>
       </footer>
     </>
   );
